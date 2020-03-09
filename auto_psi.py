d35e3beb3d9811ac23e02ab3bd57ecd896e64bf5
Alex Entwistle <alexentwistleseo@gmail.com>
2020-03-09 12:51:43 +0000

    Call API and output results to local JSON file.

--- auto_psi.py
+++ auto_psi.py
@@ -6,3 +6,12 @@ import time
 from google.colab import files
 import io
 
+
+# Define URL  
+#url = input("Enter the URL you'd like to test: ")
+url = "https://www.marketreach.co.uk/"
+
+# API request url
+r = requests.get('https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url={}/&strategy=mobile'\
+                                 .format(url))
+print(r.text)
--- single.py
+++ single.py
@@ -5,9 +5,21 @@ import requests
 import io
 
 # Define URL  
-url = input("Enter the URL you'd like to test: ")
+#url = input("Enter the URL you'd like to test: ")
+url = "https://www.marketreach.co.uk/"
 
 # API request url
 r = requests.get('https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url={}/&strategy=mobile'\
                                  .format(url))
-print(r.text)
+#print(r.text)
+
+
+
+# Convert to json format
+r_json = json.loads(r.text)
+
+print(r_json)
+
+with open('result.json', 'w') as outfile:
+  json.dump(r_json, outfile)
+
